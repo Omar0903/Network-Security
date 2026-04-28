43fb33493e3f21a0c61a4f1d9af3f64ddb902f4e
Omar0903 <3mr2brahim932004@gmail.com>
2026-04-28 23:13:26 +0300

    MS2 done

---
+++ Security T1/src/main/java/Security/CaeserCipher.java
@@ -0,0 +1,39 @@
+package Security;
+import java.util.*;
+
+public class CaeserCipher {
+    public String encrypt(String plainText, int key) {
+        plainText = plainText.toLowerCase();
+        char[] charArr = plainText.toCharArray();
+
+        for (int i = 0; i < charArr.length; i++) {
+            // Formula: E(x) = (x + k) mod 26
+            charArr[i] = (char) ('a' + (charArr[i] - 'a' + key) % 26);
+        }
+        return new String(charArr);
+    }
+
+    public String decrypt(String cipherText, int key) {
+        cipherText = cipherText.toLowerCase();
+        char[] charArr = cipherText.toCharArray();
+
+        for (int i = 0; i < charArr.length; i++) {
+            // Formula: D(x) = (x - k + 26) mod 26
+            charArr[i] = (char) ('a' + (charArr[i] - 'a' - key + 26) % 26);
+        }
+        return new String(charArr);
+    }
+
+    public int analyse(String plainText, String cipherText) {
+    plainText = plainText.toLowerCase();
+    cipherText = cipherText.toLowerCase();
+
+    for (int key = 0; key < 26; key++) {
+        if (encrypt(plainText, key).equals(cipherText)) {
+            return key;
+        }
+    }
+
+    return -1;
+}
+}
